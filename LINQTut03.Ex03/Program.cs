2c50b50eac1c66b051c5e8532a75436b233f12fc
assad <assad@MOHAMMADASSAD>
2026-06-17 23:58:48 +0300

    updated

--- LINQTut03.Ex03/Program.cs
+++ LINQTut03.Ex03/Program.cs
@@ -11,8 +11,7 @@ namespace LINQTut03.Ex03
         {
 
 
-            IEnumerable;
-            IQueryable
+            
             List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
             var evenNumbersUsingExtensionWhere =
